e8c724fd24970cebb856170e552613c0fe9885c3
DejanNikolic003 <knolekjhnny@gmail.com>
2026-02-11 10:39:34 +0100

    Add onClick prop to component

--- frontend/src/components/ButtonWithIcon.jsx
+++ frontend/src/components/ButtonWithIcon.jsx
@@ -1,12 +1,14 @@
-const ButtonWithIcon = ({ children, icon}) => {
-    return (
-        <button type="button" className="font-medium uppercase leading- w-full bg-cyan-500 hover:bg-cyan-600 text-white py-2 px-2 rounded-lg transition duration-200 cursor-pointer flex items-center justify-between">
-            {children}
-            <div className="w-5 h-5 inline-block mr-2">
-                {icon}
-            </div>
-        </button>
-    )
+const ButtonWithIcon = ({ children, icon, onClick }) => {
+  return (
+    <button
+      type="button"
+      className="font-medium uppercase leading- w-full bg-cyan-500 hover:bg-cyan-600 text-white py-2 px-2 rounded-lg transition duration-200 cursor-pointer flex items-center justify-between"
+      onClick={onClick}
+    >
+      {children}
+      <div className="w-5 h-5 inline-block mr-2">{icon}</div>
+    </button>
+  );
 };
 
-export default ButtonWithIcon
+export default ButtonWithIcon;
