62d59f31ee76d870fe79928ed9ce5944dd6ee4c7
DejanNikolic003 <knolekjhnny@gmail.com>
2026-02-11 10:39:45 +0100

    Added value, onChange prop to component

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,12 +1,16 @@
-const Input = ({ type, placeholder, icon }) => {
-    return (
-        <div className="flex items-center border border-slate-200 rounded-lg mb-2 p-2">
-            <div className="w-6 h-6 text-slate-500">
-                {icon}
-            </div>
-            <input type={type} placeholder={placeholder} className="ml-2 w-full outline-none text-slate-500 text-sm" />
-        </div>
-    )
+const Input = ({ type, placeholder, icon, value, onChange }) => {
+  return (
+    <div className="flex items-center border border-slate-200 rounded-lg mb-2 p-2">
+      <div className="w-6 h-6 text-slate-500">{icon}</div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="ml-2 w-full outline-none text-slate-500 text-sm"
+      />
+    </div>
+  );
 };
 
 export default Input;
