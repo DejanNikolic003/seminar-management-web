b127e42f19da086200261c64c5ebfe308b8464b1
DejanNikolic003 <knolekjhnny@gmail.com>
2026-02-12 15:31:34 +0100

    Remove leading class

--- frontend/src/components/ButtonWithIcon.jsx
+++ frontend/src/components/ButtonWithIcon.jsx
@@ -4,7 +4,7 @@ const ButtonWithIcon = ({ children, icon, onClick, loading }) => {
   return (
     <button
       type="button"
-      className={`font-medium uppercase leading- w-full bg-cyan-500 hover:bg-cyan-600 text-white py-2 px-2 rounded-lg transition duration-200 cursor-pointer flex items-center ${loading ? "justify-center" : "justify-between"}`}
+      className={`font-medium uppercase w-full bg-cyan-500 hover:bg-cyan-600 text-white py-2 px-2 rounded-lg transition duration-200 cursor-pointer flex items-center ${loading ? "justify-center" : "justify-between"}`}
       onClick={onClick}
     >
       {loading ? (
