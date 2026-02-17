9c5b0f262da7fa2b793da02603bd912702735b69
DejanNikolic003 <knolekjhnny@gmail.com>
2026-02-17 10:03:04 +0100

    Add logout route

--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { register, login, refresh } from "../controllers/auth.controller.js";
+import { register, login, logout, refresh } from "../controllers/auth.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.get("/refresh", refresh);
+authRouter.post("/logout", isAuthenticated, logout);
 
 export default authRouter;
