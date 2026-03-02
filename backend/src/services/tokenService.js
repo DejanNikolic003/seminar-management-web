ce6be2cce80c10648b5b9312f4c81fbf5ea8757b
DejanNikolic003 <knolekjhnny@gmail.com>
2026-03-02 14:19:15 +0100

    Add access, refresh token creation

---
+++ backend/src/services/tokenService.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { REFRESH_TOKEN, ACCESS_TOKEN, REFRESH_TOKEN_EXPIRATION, ACCESS_TOKEN_EXPIRATION } from "../constants/token";
+
+const generateAccessToken = (data) => { return jwt.sign(data, ACCESS_TOKEN, { expiresIn: ACCESS_TOKEN_EXPIRATION }) };
+const generateRefreshToken = (data) => { return jwt.sign(data, REFRESH_TOKEN, { expiresIn: REFRESH_TOKEN_EXPIRATION }) };
+const generateTokenPair = (data) => {
+    const accessToken = generateAccessToken(data);
+    const refreshToken = generateRefreshToken(data);
+
+    return { accessToken, refreshToken };
+};
+
+export { generateAccessToken, generateRefreshToken, generateTokenPair };
