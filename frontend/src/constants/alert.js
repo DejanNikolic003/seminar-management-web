1046a1931ebc820e08985b7f93adb1aeb737a2e4
DejanNikolic003 <knolekjhnny@gmail.com>
2026-03-16 14:22:24 +0100

    add constants

---
+++ frontend/src/constants/alert.js
@@ -0,0 +1,6 @@
+export const STYLES = {
+    success: "border-green-300 bg-green-50 text-green-700",
+    info: "border-blue-300 bg-blue-50 text-blue-700",
+    warning: "border-yellow-300 bg-yellow-50 text-yellow-700",
+    error: "border-red-300 bg-red-50 text-red-700",
+};
