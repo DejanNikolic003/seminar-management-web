68ae4ffe0d67c8870cf9b1fea4c400a4e3c0d0a4
DejanNikolic003 <knolekjhnny@gmail.com>
2026-03-16 14:43:20 +0100

    Add routes

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,8 +1,37 @@
-function App() {
+import { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "../routes/AppRoutes";
+import { AuthProvider } from "../context/AuthProvider";
+import FullScreenLoader from "../components/FullScreenLoader";
+
+const App = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (
+            document.readyState === "complete" ||
+            document.readyState === "interactive"
+        ) {
+            setLoading(false);
+            return;
+        }
+
+        const handleLoad = () => setLoading(false);
+        document.addEventListener("DOMContentLoaded", handleLoad);
+
+        return () => document.removeEventListener("DOMContentLoaded", handleLoad);
+    }, []);
+
+    if (loading) {
+        return <FullScreenLoader />;
+    }
+
   return (
-    <>
-        <h1>Page</h1>
-    </>
+      <AuthProvider>
+          <BrowserRouter>
+              <AppRoutes />
+          </BrowserRouter>
+      </AuthProvider>
   )
 }
 
