d52cc3e6a2b19f89c60f7dafd6cca3b17888ca0e
DejanNikolic003 <knolekjhnny@gmail.com>
2026-03-16 15:01:06 +0100

    Add secret from .env file

--- backend/src/constants/token.js
+++ backend/src/constants/token.js
@@ -1,5 +1,5 @@
-export const ACCESS_TOKEN = "ACCESS";
-export const REFRESH_TOKEN = "REFRESH";
+export const ACCESS_TOKEN = process.env.ACCESS_TOKEN_SECRET || "ACCESS";
+export const REFRESH_TOKEN = process.env.REFRESH_TOKEN_SECRET || "REFRESH";
 
 export const ACCESS_TOKEN_EXPIRATION = "1h";
 export const REFRESH_TOKEN_EXPIRATION = "7d";
