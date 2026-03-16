250a6b77d0ee57981113a63cd7f09218e9849dad
DejanNikolic003 <knolekjhnny@gmail.com>
2026-03-16 14:40:49 +0100

    Login and Register page

---
+++ frontend/src/pages/auth/forms/LoginForm.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import useAuth from "../../../hooks/useAuth";
+import { KeyRound, LogIn, Mail } from "lucide-react";
+import Input from "../../../components/Input";
+import ButtonWithIcon from "../../../components/ButtonWithIcon";
+import Alert from "../../../components/Alert";
+import { useNavigate } from "react-router-dom";
+
+const LoginForm = () => {
+    const { login } = useAuth();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({});
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setLoading(true);
+
+        try {
+            await login({ email, password });
+            navigate("/");
+        } catch (error) {
+            setError({ message: error.message });
+            setEmail("");
+            setPassword("");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <form className="px-6 pb-4">
+            {error.message && <Alert type="error">{error.message}</Alert>}
+            <div className="mb-2">
+                <Input
+                    type={"email"}
+                    placeholder={"E-mail adresa"}
+                    icon={<Mail />}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="mb-2">
+                <Input
+                    type={"password"}
+                    placeholder={"Lozinka"}
+                    icon={<KeyRound />}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <ButtonWithIcon icon={<LogIn />} onClick={handleSubmit} loading={loading}>
+                Prijavi se
+            </ButtonWithIcon>
+        </form>
+    );
+};
+
+export default LoginForm;
---
+++ frontend/src/pages/auth/forms/RegisterForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { KeyRound, LogIn, Mail, User } from "lucide-react";
+import Input from "../../../components/Input";
+import ButtonWithIcon from "../../../components/ButtonWithIcon";
+import Alert from "../../../components/Alert";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+
+const RegisterForm = () => {
+    const { register } = useAuth();
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({});
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            await register({ firstName, lastName, email, password });
+
+            navigate("/");
+        } catch (error) {
+            setError({ message: error.message });
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <form className="px-6 pb-4">
+            {error.message && <Alert type="error">{error.message}</Alert>}
+
+            <div className="md:flex gap-2">
+                <Input
+                    type={"text"}
+                    placeholder={"Ime"}
+                    icon={<User />}
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <Input
+                    type={"text"}
+                    placeholder={"Prezime"}
+                    icon={<User />}
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <Input
+                type={"email"}
+                placeholder={"E-mail adresa"}
+                icon={<Mail />}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+                type={"password"}
+                placeholder={"Lozinka"}
+                icon={<KeyRound />}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <ButtonWithIcon icon={<LogIn />} onClick={handleSubmit} loading={loading}>
+                Registruj se
+            </ButtonWithIcon>
+        </form>
+    );
+};
+
+export default RegisterForm;
