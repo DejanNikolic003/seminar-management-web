0050eea939fcbb5b138f654ee8d17c8e43e96c8d
DejanNikolic003 <knolekjhnny@gmail.com>
2026-05-06 18:45:15 +0200

    tiny fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import isAuthenticated from "./middleware/isAuthenticated.js";
 import authRouter from "./routes/authRoutes.js";
 import subjectRouter from "./routes/subjectRoutes.js";
@@ -9,13 +8,9 @@ import seminarRouter from "./routes/seminarRoutes.js";
 import enrollmentRouter from "./routes/enrollmentRoutes.js";
 
 const app = express();
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRouter);
 app.use(isAuthenticated);
