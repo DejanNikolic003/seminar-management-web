5e963ac6a682e5ec59b675c459d446f9567961ed
DejanNikolic003 <knolekjhnny@gmail.com>
2026-05-08 12:41:47 +0200

    add admin role

--- backend/src/middleware/isAuthorized.js
+++ backend/src/middleware/isAuthorized.js
@@ -2,11 +2,13 @@ import * as ROLES from "../constants/roles.js";
 
 const isAuthorized = (role) => {
     return (req, res, next) => {
-        if(![ROLES.STUDENT, ROLES.PROFESSOR].includes(role))
+        const allowedRoles = Array.isArray(role) ? role : [role];
+
+        if (!allowedRoles.every(currentRole => [ROLES.ADMIN, ROLES.STUDENT, ROLES.PROFESSOR].includes(currentRole)))
            return res.status(404).json({ message: "Tražena grupa permisija ne postoji." });
 
         const { role: userRole } = req.user;
-        if(userRole !== role)
+        if(!allowedRoles.includes(userRole))
             return res.status(401).json({ message: "Niste ovlašćeni!" });
 
         next();
