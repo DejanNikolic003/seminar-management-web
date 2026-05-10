27f4c7c6d0ee5e982ec5dad17d13b473be981ee5
DejanNikolic003 <knolekjhnny@gmail.com>
2026-05-10 10:52:53 +0200

    Add enrollStudentByEmail fn

--- frontend/src/hooks/useSubjects.jsx
+++ frontend/src/hooks/useSubjects.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import useAxiosPrivate from "./useAxiosPrivate";
-import useAuth from "./useAuth";
 
 const useSubjects = () => {
     const axiosPrivate = useAxiosPrivate();
-    const { auth } = useAuth();
     const [subjects, setSubjects] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -52,101 +50,12 @@ const useSubjects = () => {
         }
     };
 
-    const fetchTopicsBySubject = async (subjectId) => {
-        try {
-            const { data: response } = await axiosPrivate.get(`/topics/subject/${subjectId}`);
-            return response.topics;
-        } catch (error) {
-            const message =
-                error.response?.data?.message ||
-                "Došlo je do greške prilikom dohvatanja tema!";
-            throw new Error(message);
-        }
-    };
-
-    const createTopic = async (subjectId, name, description) => {
-        try {
-            const { data: response } = await axiosPrivate.post("/topics", {
-                subject_id: Number(subjectId),
-                name,
-                description,
-            });
-
-            return response;
-        } catch (error) {
-            const message =
-                error.response?.data?.message ||
-                "Došlo je do greške prilikom dodavanja teme!";
-            throw new Error(message);
-        }
-    };
-
-    const updateTopic = async (topicId, name, description) => {
-        try {
-            const { data: response } = await axiosPrivate.put(`/topics/${topicId}`, {
-                name,
-                description,
-            });
-
-            return response;
-        } catch (error) {
-            const message =
-                error.response?.data?.message ||
-                "Došlo je do greške prilikom izmene teme!";
-            throw new Error(message);
-        }
-    };
-
-    const deleteTopic = async (topicId) => {
-        try {
-            const { data: response } = await axiosPrivate.delete(`/topics/${topicId}`);
-            return response;
-        } catch (error) {
-            const message =
-                error.response?.data?.message ||
-                "Došlo je do greške prilikom brisanja teme!";
-            throw new Error(message);
-        }
-    };
-
-    const reserveTopic = async (topicId, subjectId) => {
-        try {
-            const { data: response } = await axiosPrivate.post(`/topics/${topicId}/reserve`, {
-                subject_id: Number(subjectId),
-            });
-            return response;
-        } catch (error) {
-            const message =
-                error.response?.data?.message ||
-                "Došlo je do greške prilikom rezervacije teme!";
-            throw new Error(message);
-        }
-    };
-
-    const fetchSeminarsBySubject = async (subjectId) => {
-        try {
-            const { data: response } = await axiosPrivate.get(`/seminars?subject_id=${subjectId}`);
-            return response.seminars;
-        } catch (error) {
-            const message =
-                error.response?.data?.message ||
-                "Došlo je do greške prilikom dohvatanja rezervacija!";
-            throw new Error(message);
-        }
-    };
-
     return {
         subjects,
         loading,
         fetchSubjects,
         fetchSubject,
         enrollStudentByEmail,
-        fetchTopicsBySubject,
-        createTopic,
-        updateTopic,
-        deleteTopic,
-        reserveTopic,
-        fetchSeminarsBySubject,
     };
 };
 
