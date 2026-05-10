65b04b73cec5021e8feddd1a3407890452d7f26f
DejanNikolic003 <knolekjhnny@gmail.com>
2026-05-10 10:54:13 +0200

    Add uploading seminars

---
+++ frontend/src/hooks/useSeminars.jsx
@@ -0,0 +1,71 @@
+import useAxiosPrivate from "./useAxiosPrivate";
+
+const useSeminars = () => {
+    const axiosPrivate = useAxiosPrivate();
+
+    const fetchSeminarsBySubject = async (subjectId) => {
+        try {
+            const { data: response } = await axiosPrivate.get(`/seminars?subject_id=${subjectId}`);
+            return response.seminars;
+        } catch (error) {
+            const message =
+                error.response?.data?.message ||
+                "Došlo je do greške prilikom dohvatanja rezervacija!";
+            throw new Error(message);
+        }
+    };
+
+    const fetchSeminarByTopic = async (topicId) => {
+        try {
+            const { data: response } = await axiosPrivate.get(`/seminars/topic/${topicId}`);
+            return response.seminar;
+        } catch (error) {
+            const message =
+                error.response?.data?.message ||
+                "Došlo je do greške prilikom dohvatanja seminarskog rada!";
+            throw new Error(message);
+        }
+    };
+
+    const uploadSeminarPaper = async (topicId, file) => {
+        try {
+            const formData = new FormData();
+            formData.append("file", file);
+
+
+            const { data: response } = await axiosPrivate.put(
+                `/seminars/topic/${topicId}/upload`,
+                formData
+            );
+            return response;
+        } catch (error) {
+            const message =
+                error.response?.data?.message ||
+                "Došlo je do greške prilikom postavljanja seminarskog rada!";
+            throw new Error(message);
+        }
+    };
+
+    const updateSeminarStatus = async (seminarId, status) => {
+        try {
+            const { data: response } = await axiosPrivate.put(`/seminars/${seminarId}/status`, {
+                status,
+            });
+            return response;
+        } catch (error) {
+            const message =
+                error.response?.data?.message ||
+                "Došlo je do greške prilikom promene statusa seminarskog rada!";
+            throw new Error(message);
+        }
+    };
+
+    return {
+        fetchSeminarsBySubject,
+        fetchSeminarByTopic,
+        uploadSeminarPaper,
+        updateSeminarStatus,
+    };
+};
+
+export default useSeminars;
